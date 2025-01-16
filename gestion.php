ee4feeeaba0689719cde376318405b1d0ae618c2
Maralde <marcosallet@gmail.com>
2025-01-16 10:05:31 +0100

    Creado los archivos para empezar a crear las pantallas

---
+++ gestion.php
@@ -0,0 +1,26 @@
+<?php
+require_once('./php/configuration.php');
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Gestion Catálogo</title>
+    <link rel="stylesheet" href="./styles/gestion.css">
+</head>
+<body>
+    <nav>
+        <div class="nav-profile">
+            <ul class="nav-list">
+                <li>
+                    <input type="search" name="vinylSearch" id="vinylSearch">
+                </li>
+                <li>
+                    <a href="newVinyl.php">Añadir Vinilo</a>
+                </li>
+            </ul>
+        </div>
+    </nav>
+</body>
+</html>
